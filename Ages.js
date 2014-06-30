eae8b636fa83dd1ebe3e8eb5d61980f1f8faf503
AlexanderBaschuk <koshka85@mail.ru>
2014-06-30 16:42:53 +0400

    Изменена ось У, добавлены линии

--- Ages.js
+++ Ages.js
@@ -1,12 +1,12 @@
 /*global d3:false */
 var w = 800;
-var h = 200;
+var h = 250;
 var dataset;
 var currentData = [];
 var currentYear = "2009";
 var graph = {
 	paddingLeft: 30,
-	paddingTop: 10,
+	paddingTop: 20,
 	paddingRight: 10,
 	paddingBottom: 20,
 	colorGirls: "green",
@@ -16,7 +16,6 @@ var xScale;
 var hScale;
 var yScale;
 var yearScale;
-var yAxis;
 
 var years = {
 	"2009": {next: "2011", exists: 1},
@@ -28,6 +27,7 @@ var years = {
 };
 
 var yearsArray = ["2009", "2010", "2011", "2012", "2013", "2014"];
+var yStops = [0, 10, 20, 30, 40, 50];
 
 d3.csv("Ages.csv", function (error, data) {
 	"use strict";
@@ -81,7 +81,7 @@ function setScalesAndAxes() {
 		.rangeBands([graph.paddingLeft, w - graph.paddingRight], 0.1, 0.2);
 
 	var hDomainRightOrig = d3.max(dataset, function (d) { return d.girlsTotal + d.boysTotal; });
-	var hDomainRightNew = Math.ceil(hDomainRightOrig / 10.0) * 10;
+	var hDomainRightNew = Math.ceil(hDomainRightOrig / 10.0) * 10 + 2;
 	hScale = d3.scale.linear()
 		.domain([0, hDomainRightNew])
 		.range([0, h - graph.paddingBottom - graph.paddingTop]);
@@ -90,11 +90,6 @@ function setScalesAndAxes() {
 		.domain([0, hDomainRightNew])
 		.range([h - graph.paddingBottom + 0.5, graph.paddingTop]);
 	
-	yAxis = d3.svg.axis()
-		.scale(yScale)
-		.orient("left")
-		.ticks(6);
-		
 	yearScale = d3.scale.ordinal()
 		.domain(yearsArray)
 		.rangeBands([graph.paddingLeft + (w - graph.paddingLeft - graph.paddingRight) / 2, w - graph.paddingRight]);
@@ -107,12 +102,28 @@ function generateVis() {
 		.attr("width", w)
 		.attr("height", h);
 
+	// Фоновый прямоугольник.
 	svg.append("rect").attr({
 		height: h,
 		width: w,
 		fill: "rgb(220,220,240)"
 	});
+		
+	// Промежуточные горизонтальные линии.
+	svg.append("g")
+		.attr("class", "intLine")
+		.selectAll(".hLine")
+		.data(yStops)
+		.enter()
+		.append("line")
+		.attr("class", "hLine")
+		.attr("x1", graph.paddingLeft)
+		.attr("x2", w - graph.paddingRight)
+		.attr("y1", function (d) { return yScale(d); })	
+		.attr("y2", function (d) { return yScale(d); })		
+		.attr("style", "stroke:rgb(200,200,200);stroke-width:1");
 
+	// Ось X.
 	svg.append("line").attr({
 		x1: graph.paddingLeft,
 		x2: w - graph.paddingRight,
@@ -121,6 +132,7 @@ function generateVis() {
 		style: "stroke:rgb(100,100,100);stroke-width:1"
 	});
 
+	// Ось Y.
 	svg.append("line").attr({
 		x1: graph.paddingLeft,
 		x2: graph.paddingLeft,
@@ -128,12 +140,49 @@ function generateVis() {
 		y2: h - graph.paddingBottom + 0.5,
 		style: "stroke:rgb(100,100,100);stroke-width:1"
 	});
-
+	
+	// Подписи X.
 	svg.append("g")
 		.attr("class", "label")
-		.attr("transform", "translate(" + graph.paddingLeft + ", 0)")
-		.call(yAxis);
+		.selectAll(".ageLabel")
+		.data(currentData, function (d) { return d.age; })
+		.enter()
+		.append("text")
+		.attr("class", "ageLabel")
+		.attr("dx", function (d) { return xScale(d.age) + xScale.rangeBand() / 2; })
+		.attr("dy", h - graph.paddingBottom + 10)
+		//.text(function (d) { return d.age % 2 === 0 ? d.age : " "; });
+		.text(function (d) { return (d.age === 3 || d.age % 5 === 0) ? d.age : " "; });
 
+	// Подписи оси Y
+	svg.append("g")
+		.attr("class", "label")
+		.selectAll(".yLabel")
+		.data(yStops)
+		.enter()
+		.append("text")
+		.attr("class", "yLabel")
+		.attr("dx", graph.paddingLeft - 5)
+		.attr("dy", function (d) { return yScale(d); })
+		.attr("style", "dominant-baseline: central;")
+		.text(function (d) { return d; });
+		
+	// 2009 2010 2011 2012 ...
+	svg.append("g")
+		.attr("class", "yearLabel")
+		.selectAll("text")
+		.data(yearsArray)
+		.enter()
+		.append("text")
+		.append("tspan")
+		.attr("dx", function(d, i) { return w/2 + i * 60 + 50; })
+		.attr("dy", (yScale(40) + yScale(50)) / 2)
+		.attr("style", "dominant-baseline: central;")		
+		.text(function (d) { return d; });
+
+	updateYearLabels(svg);
+	
+	// Столбики
 	var bars = svg.append("g");
 	
 	bars.selectAll(".barG")
@@ -157,31 +206,7 @@ function generateVis() {
 		.attr("height", function (d) { return hScale(d.boysTotal); })
 		.attr("width", xScale.rangeBand())
 		.attr("fill", graph.colorBoys);
-
-	svg.append("g")
-		.attr("class", "label")
-		.selectAll(".ageLabel")
-		.data(currentData, function (d) { return d.age; })
-		.enter()
-		.append("text")
-		.attr("class", "ageLabel")
-		.attr("dx", function (d) { return xScale(d.age) + xScale.rangeBand() / 2; })
-		.attr("dy", h - graph.paddingBottom + 10)
-		.text(function (d) { return d.age % 2 === 0 ? d.age : " "; });
 		
-	svg.append("g")
-		.attr("class", "yearLabel")
-		.selectAll("text")
-		.data(yearsArray)
-		.enter()
-		.append("text")
-		.append("tspan")
-		.attr("dx", function(d) { return yearScale(d); })
-		.attr("dy", graph.paddingTop + 15)
-		.text(function (d) { return d; });
-
-	updateYearLabels(svg);
-	
 	svg.on("click", function () {
 		setNextYear();
 		setCurrentData();
@@ -198,13 +223,11 @@ function updateBars(svg) {
 		.transition()
 		.attr("y", function (d) { return h - graph.paddingBottom - hScale(d.girlsTotal + d.boysTotal); })
 		.attr("height", function (d) { return hScale(d.girlsTotal); });
-		//.each("end", function () { updateYearLabels(svg); });
 
 	svg.selectAll(".barB")
 		.data(currentData, function (d) { return d.age; })
 		.transition()
 		.attr("y", function (d) { return h - graph.paddingBottom - hScale(d.boysTotal); })
 		.attr("height", function (d) { return hScale(d.boysTotal); });
-		//.each("end", function () { updateYearLabels(svg); });
 }
 
